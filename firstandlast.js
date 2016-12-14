5916c67a8b83ff72995bb19f5a8588b32386f101
Christopher Baek <baekc@amazon.com>
2016-12-13 21:07:11 -0800

    Convert HTML exercises to JS

---
+++ firstandlast.js
@@ -0,0 +1,10 @@
+/*
+
+First and Last
+
+Write a JavaScript function called first() that takes one input, an array, and returns the first element in that array
+Write another JavaScript function called last() that takes one input, an array, and returns the lastt element in that array
+
+*/
+
+// YOUR CODE GOES HERE
---
+++ longeststring.js
@@ -0,0 +1,10 @@
+/*
+
+Longest String
+
+Given an array of strings, find the longest string and print out that string.
+hint: strings also have the .length property - i.e `"starburst".length` is 9
+
+*/
+
+// YOUR CODE GOES HERE
